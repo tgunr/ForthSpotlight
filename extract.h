f8be5e249d4d0736e8f71e2f3b0aef49e1f5825e
Dave Carlton <davec@polymicrosystems.com>
2010-08-01 12:52:58 -0700

    Corrected header.

--- extract.h
+++ extract.h
@@ -3,7 +3,7 @@
  *  Forth Spotlighter
  *
  *  Created by Dave on 5/10/05.
- *  Copyright 2005 __MyCompanyName__. All rights reserved.
+ *  Copyright 2005 PolyMicro Systems. All rights reserved.
  *
  */
 
